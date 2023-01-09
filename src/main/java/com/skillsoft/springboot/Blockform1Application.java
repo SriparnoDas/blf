49296ed56e0eb31d915677713b0043e2d70f5f1d
Sriparno Das <sriparno_das@hcl.com>
2023-01-09 17:24:33 +0530

    First Commit

---
+++ src/main/java/com/skillsoft/springboot/Blockform1Application.java
@@ -0,0 +1,13 @@
+package com.skillsoft.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blockform1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blockform1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillsoft/springboot/Blockform1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsoft.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Blockform1ApplicationTests {
+	
+	@Test
+	void contextLoads() {
+		
+	}
+}
